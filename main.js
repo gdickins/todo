8bf2577777cd052b43a4ce4c8a9847062080a02e
Gabe Dickinson <gabe.dickinson@gmail.com>
2016-07-06 22:00:07 -0500

    barebones working

--- main.js
+++ main.js
@@ -6,21 +6,34 @@ $(document).ready(function(){
     success: function(response) {
       response.forEach(function(listItem){
         var $li = $('<li></li>');
-        var $but = $('<button class="delete">Delete</button>');
+        var $but = $('<button>Delete</button>').attr('class',listItem._id);
         $li.text(listItem.input);
-        $li.prepend($but);
+        $li.prepend($but).on('click', function(evt) {
+                console.log(evt);
+                evt.target.parentElement.remove();
+                $.ajax({
+                url: 'http://tiny-za-server.herokuapp.com/collections/gabe/'+listItem._id,
+                type: 'DELETE',
+                dataType: 'json',
+                success: function (response) {
+                  console.log('delete', response);
+              }
+            });
+          });
         $('#listDisp').append($li);
       });
     }
   });
-  $('#addBox').on('submit', function(evt) {
+  $('#addBox').on('submit', function(evt) {         //Start of the submit event
     evt.preventDefault();
     var input = $('#inputBox').val();
     var $li = $('<li></li>');
     var $but = $('<button class="delete">Delete</button>');
     $li.text(input);
-    $li.prepend($but);
-    $('#listDisp').append($li);
+    $li.prepend($but).on('click', function(evt) {
+      console.log('click', evt);
+    });
+    $('#listDisp').prepend($li);
     $.ajax({
       url: 'http://tiny-za-server.herokuapp.com/collections/gabe',
       type: 'POST',
